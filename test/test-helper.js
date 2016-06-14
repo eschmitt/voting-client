c54634ea9470dfaf984c4fd29a395e62e813218b
Eric Schmitt <ericschmitt1@gmail.com>
2016-06-14 17:23:13 -0400

    Add immutable and chai-immutable to dependency lists. Use chai-immutable in test helper.

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,5 +1,7 @@
 import jsdom from 'jsdom';
 import {merge, identity} from 'ramda';
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
 
 const doc = jsdom.jsdom('<!DOCTYPE html><html><body></body></html>');
 const win = doc.defaultView;
@@ -8,3 +10,5 @@ global.document = doc;
 global.window = win;
 
 global = mergeWith(identity, global, window);
+
+chai.use(chaiImmutable);
