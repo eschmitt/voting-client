4739d15c3904958cbc6b747679da1815eeca7418
Eric Schmitt <ericschmitt1@gmail.com>
2016-06-25 08:44:37 -0400

    Add index file, the main entry point to the client app.

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,14 +1,21 @@
-import jsdom from 'jsdom';
-import {merge, identity} from 'ramda';
+import {jsdom} from 'jsdom';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
 
-const doc = jsdom.jsdom('<!DOCTYPE html><html><body></body></html>');
+const doc = jsdom('<!DOCTYPE html><html><body></body></html>');
 const win = doc.defaultView;
 
 global.document = doc;
 global.window = win;
 
-global = mergeWith(identity, global, window);
+Object.keys(window).forEach((key) => {
+  if (!key in global) {
+    global[key] = window[key];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
 
 chai.use(chaiImmutable);
